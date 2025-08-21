a25999f53ae50ef3c9ed022322382796002594e4
parthsangale123 <parthsangale325@gmail.com>
2025-08-21 10:33:55 +0530

    merging

--- SGS&C Hackathon Game/Assets/Game Assets/_Scripts/MainMenu.cs
+++ SGS&C Hackathon Game/Assets/Game Assets/_Scripts/MainMenu.cs
@@ -217,7 +217,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Main Menu");
     }
-
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMainMenu");
+    }
 
     public void ResetGateKnowledge()
     {
